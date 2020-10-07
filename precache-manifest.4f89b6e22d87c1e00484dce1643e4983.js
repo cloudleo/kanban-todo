78b46a3b4d65717605330eba44812a3c781f83db
leonardo <l.castillo1989@Hotmail.com>
2020-10-07 11:20:22 -0400

    Updates

---
+++ precache-manifest.4f89b6e22d87c1e00484dce1643e4983.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3109ef89096b75cf3979328fbd9cec27",
+    "url": "/kanban-todo/index.html"
+  },
+  {
+    "revision": "ab077ee359b72b9cd2b9",
+    "url": "/kanban-todo/static/css/main.87495619.chunk.css"
+  },
+  {
+    "revision": "fa218a08ec94baf9257e",
+    "url": "/kanban-todo/static/js/2.9275f874.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanban-todo/static/js/2.9275f874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab077ee359b72b9cd2b9",
+    "url": "/kanban-todo/static/js/main.121f297e.chunk.js"
+  },
+  {
+    "revision": "6aa0d4f5dd6fb17005bc",
+    "url": "/kanban-todo/static/js/runtime-main.75f7f695.js"
+  }
+]);
